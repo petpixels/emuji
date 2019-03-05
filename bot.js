00e8dc683a455151a588bb82dbcca62965d382b2
root <Alan Underwood>
2019-03-05 10:17:05 +0000

    Brand new emu

---
+++ bot.js
@@ -0,0 +1,288 @@
+// bot.js
+var bot = function () {};
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+// When it says secret, it really kind of means secret...
+// so let's put it right at the top of this file here
+const bot_secret = require('./bot-secret')
+
+var bot_name
+var bot_rating
+var bot_reply
+var bot_keywords
+var bot_odds
+
+const fs = require('fs');
+const request = require('request');
+
+const { createLogger, format, transports } = require('winston');
+const env = process.env.NODE_ENV || 'development';
+
+require('winston-daily-rotate-file');
+
+const conf_dir = "conf"; // log directory
+const log_dir = "logs"; // log directory
+const log_file = "bot"  // log file name
+
+// Create the log & conf directories if it does not exist
+if (!fs.existsSync(log_dir)) { fs.mkdirSync(log_dir); }
+if (!fs.existsSync(conf_dir)) { fs.mkdirSync(conf_dir); }
+
+// const filename = path.join(log_dir, 'emuji.log');
+const dailyRotateFileTransport = new transports.DailyRotateFile({
+  filename: `${log_dir}/${log_file}-%DATE%.log`,
+  datePattern: 'YYYY-MM-DD'
+});
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    dailyRotateFileTransport
+    //new transports.File({ filename })
+  ]
+});
+
+bot.prototype.name = function(name) {
+  bot.bot_name = name
+  logger.info("@" + bot.bot_name + ": Hello, my name is " + bot.bot_name)
+}
+
+bot.prototype.keywords = function(input) {
+  bot.keywords = input.toLowerCase()
+  logger.info("@" + bot.bot_name + ": Keywords changed to " + bot.keywords)
+}
+
+bot.prototype.default_reply = function(reply) {
+  bot.bot_reply = reply
+  logger.info("@" + bot.bot_name + ": Default reply changed to " + bot.bot_reply)
+}
+
+bot.prototype.odds = function(percent) {
+  bot.odds = percent
+  logger.info("@" + bot.bot_name + ": Odds set to " + bot.odds.replace(".","") + "%")
+}
+
+bot.prototype.reply = function(channel, input) {
+  var retString = bot.bot_reply
+
+  if (!(input)) {
+    // set default odds to 25%
+    if (!(bot.odds)) {
+      bot.odds = .25
+      logger.info("@" + bot.bot_name + ": Odds defaulted to " + bot.odds + "%")
+    }
+
+    var roll = Math.random()
+    if (roll < bot.odds) {
+      var nowPlaying = []
+      var filePlaying = "./" + conf_dir + "/reply.txt"
+      var retString
+
+      var nowPlaying = fs.readFileSync(filePlaying).toString().split("\n");
+      var randomPlaying = Math.floor(Math.random() * nowPlaying.length)
+
+      for (i in nowPlaying) {
+        //console.log(nowPlaying[i]);
+        if (i == randomPlaying) {
+          retString = nowPlaying[randomPlaying]
+        }
+      }
+
+      if (!(retString)) { retString = "Error" }
+    }
+  }
+
+  var emuChan = channel
+  emuChan.send(retString)
+  logger.info("@" + bot.bot_name + ": " + retString)
+  return retString
+}
+
+bot.prototype.rating = function(rating) {
+  bot.bot_rating = rating
+  logger.info("@" + bot.bot_name + ": Rating changed to " + bot.bot_rating)
+}
+
+bot.prototype.log = function(msg) {
+  logger.info("@" + bot.bot_name + ": " + msg)
+};
+
+bot.prototype.gif = function(channel, q) {
+  if (!(q)) { q = "" } // default to empty string
+
+  var aGif = q.split(" ")
+  var gifRating = bot.bot_rating
+  var gifTag = bot.bot_name.replace("bot","")
+  var gifLoc = 0
+  var gif
+
+  // filter out !gif command
+  for (var i = 0; i < aGif.length; i++) {
+    if (aGif[i] == "!gif") {
+      gifLoc = i+1
+    }
+  }
+
+  // build giphy query URL
+  for (var i = gifLoc; i < aGif.length; i++) {
+    gifTag = gifTag + "%20" + aGif[i]
+  }
+
+  // default to G-Rated
+  if (!(gifRating)) { gifRating = "G" }
+
+  var url = "https://api.giphy.com/v1/gifs/random?api_key=" + bot_secret.giphy_api_key + "&tag=" + gifTag + "&rating=" + gifRating;
+  logger.info("@" + bot.bot_name + ": " + "Giphy request URL: " + url)
+
+  request.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      logger.debug("@" + bot.bot_name + ": " + "Giphy request failed")
+    } else if (res.statusCode !== 200) {
+      logger.debug("@" + bot.bot_name + ": " + "Giphy request succeeded")
+    } else {
+      // data is already parsed as JSON:
+      var received = data
+      // loop through each data object because there can be more than one
+      for (var data in received) {
+        var giphy = received[data];
+        gif = giphy.embed_url
+
+        if (gif) {
+          logger.info("@" + bot.bot_name + ": " + gif)
+
+          channel.send(gif)
+        }
+      }
+    }
+  });
+}
+
+bot.prototype.sticker = function(channel, q) {
+  if (!(q)) { q = "" } // default to empty string
+
+  var aSticker = q.split(" ")
+  var stickerRating = bot.bot_rating
+  var stickerTag = bot.bot_name.replace("bot","")
+  var stickerLoc = 0
+  var sticker
+
+  // filter out !gif command
+  for (var i = 0; i < aSticker.length; i++) {
+    if (aSticker[i] == "!sticker") {
+      stickerLoc = i+1
+    }
+  }
+
+  // build giphy query URL
+  for (var i = stickerLoc; i < aSticker.length; i++) {
+    stickerTag = stickerTag + "%20" + aSticker[i]
+  }
+
+  // default to G-Rated
+  if (!(stickerRating)) { stickerRating = "G" }
+
+  var url = "https://api.giphy.com/v1/stickers/random?api_key=" + bot_secret.giphy_api_key + "&tag=" + stickerTag + "&rating=" + stickerRating;
+  logger.info("@" + bot.bot_name + ": " + "Giphy request URL: " + url)
+
+  request.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      logger.debug("@" + bot.bot_name + ": " + "Giphy request failed")
+    } else if (res.statusCode !== 200) {
+      logger.debug("@" + bot.bot_name + ": " + "Giphy request succeeded")
+    } else {
+      // data is already parsed as JSON:
+      var received = data
+      // loop through each data object because there can be more than one
+      for (var data in received) {
+        var giphy = received[data];
+        sticker = giphy.embed_url
+
+        if (sticker) {
+          logger.info("@" + bot.bot_name + ": " + sticker)
+          channel.send(sticker)
+        }
+      }
+    }
+  });
+}
+
+bot.prototype.play = function(msg) {
+
+  var nowPlaying
+
+  if (msg) {
+    nowPlaying = msg
+  } else {
+    var nowPlaying = this.randomPlaying()
+  }
+
+  logger.info("@" + bot.bot_name + ": " + "Now Playing: " + nowPlaying)
+  return nowPlaying
+}
+
+bot.prototype.randomPlaying = function() {
+
+  var nowPlaying = []
+  var filePlaying = "./" + conf_dir + "/play.txt"
+  var retString
+
+  var nowPlaying = fs.readFileSync(filePlaying).toString().split("\n");
+  var randomPlaying = Math.floor(Math.random() * nowPlaying.length)
+
+  for (i in nowPlaying) {
+    //console.log(nowPlaying[i]);
+    if (i == randomPlaying) {
+      retString = nowPlaying[randomPlaying]
+    }
+  }
+
+  if (!(retString)) { retString = "Error" }
+  return retString
+}
+
+bot.prototype.randomReply = function() {
+  // atMeow[24] = this.randomCatEmoji()
+
+  var fileReplies = "./" + conf_dir + "/reply.txt"
+  var retString
+
+  var replies = fs.readFileSync(fileReplies).toString().split("\n");
+  var randomReplyNum = Math.floor(Math.random() * replies.length)
+
+  for (i in replies) {
+    //console.log(nowPlaying[i]);
+    if (i == randomReplyNum) {
+      retString = replies[randomReplyNum]
+    }
+  }
+
+  return retString
+}
+
+module.exports = bot;
--- emuji.js
+++ emuji.js
@@ -1,104 +1,59 @@
-// When it says secret, it really kind of means secret...
-// so let's put it right at the top of this file here
-const emuji_secret = require("./emuji-secret")
-const bot_secret_token = emuji_secret.bot_secret_token
+const bot_secret = require('./bot-secret')
+
+var emubot = require('./bot');
+const emuji = require("./emuji-functions")
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-const emuji = require("./emuji-functions")
+// const emuji = require("./emuji-functions")
 const emujiUserID = "Emuji#8780"
 
 // channels (probably shouldn't be hardcoded)
-// maybe create a clever algorithm that searches for a channel named emuji
-const chan_emuji = "552238617926303750"
+// maybe create a clever algorithm that searches for a channel named emu
+const chan_emu = "552238617926303750"
 
-const { createLogger, format, transports } = require('winston');
-require('winston-daily-rotate-file');
-
-const fs = require('fs');
-const path = require('path');
-const request = require('request');
-
-const env = process.env.NODE_ENV || 'development';
-const logDir = 'logs';
-
-// Create the log directory if it does not exist
-if (!fs.existsSync(logDir)) { fs.mkdirSync(logDir); }
-
-// const filename = path.join(logDir, 'emuji.log');
-const dailyRotateFileTransport = new transports.DailyRotateFile({
-  filename: `${logDir}/emuji-%DATE%.log`,
-  datePattern: 'YYYY-MM-DD'
-});
-
-const logger = createLogger({
-  // change level if in dev environment versus production
-  level: env === 'development' ? 'debug' : 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-  ),
-  transports: [
-    new transports.Console({
-      level: 'info',
-      format: format.combine(
-        format.colorize(),
-        format.printf(
-          info => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-      )
-    }),
-    dailyRotateFileTransport
-    //new transports.File({ filename })
-  ]
-});
+var emu = new emubot()
 
 process.on('uncaughtException', function(err) {
-  logger.debug(err)
-
+  emu.log(err)
+  console.log(err)
   // set discord client "now playing"
   // client.user.setActivity(emuji.play("Dead"))
 });
 
 client.on('ready', () => {
-  var emujiChannel = client.channels.get(chan_emuji)
-  var sayHello = "SQUAWK"
-  logger.info("Connected as " + client.user.tag)
+  var emuChannel = client.channels.get(chan_emu)
+  emu.log("Connected as " + client.user.tag)
+
+  emu.name("Emu")
+  emu.keywords("emu")
+  emu.default_reply("...")
+  emu.rating("G")
 
   // set discord client "now playing"
-  var startPlaying = sayHello;
-  client.user.setActivity(sayHello)
-  logger.info("Now playing " + sayHello)
+  client.user.setActivity(emu.play())
 
-  console.log("Servers:")
-  client.guilds.forEach((guild) => {
-      console.log(" - " + guild.name)
+  // send greeting to channel
+  emu.reply(emuChannel, msg="")
 
-      // List all channels
-      guild.channels.forEach((channel) => {
-          console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-      })
-  })
 })
 
 // Reply to messages
 client.on('message', (receivedMessage) => {
   var replyRequired = false
-  var suppressOutput = false;
+  var silent = false;
 
   // Prevent bot from responding to its own messages
   if (receivedMessage.author == client.user) { return } // catch and release
 
   var msg = receivedMessage.content;
+  emu.log(receivedMessage.channel + msg)
 
   // React to all messages and log each reaction
   var emuEmoji = emuji.react(receivedMessage.content)
   if (emuEmoji) {
     for (var i = 0; i < emuEmoji.length; i++) {
-      logger.info("Reacted to: <" + receivedMessage.channel.id + "> " + receivedMessage.content + " with emoji " + emuEmoji[i])
       receivedMessage.react(emuEmoji[i])
     }
   }
@@ -106,90 +61,28 @@ client.on('message', (receivedMessage) => {
   // Check if the bot's user was tagged in the message
   // Always reply to messages from any channel
   if (receivedMessage.isMentioned(client.user)) {
-    var silent = false
-
     // Get acknowledgement message from catbot
     var direct_input = receivedMessage.content.toLowerCase()
     var direct_output = "..."
 
     // Log acknowledgement message
-    logger.info("Tagged message received from " + receivedMessage.author.toString() + ": " + receivedMessage.content)
-
     var msg = receivedMessage.content;
 
-    // Really need to modularize this function...
+    // Really need to modularize this function... (Done!)
     if (msg.toLowerCase().includes("!gif")) {
       silent = true
-
-      // build URL query
-      // double iteration has got to be a bad idea
-      // but if it's stupid and it works it's not stupid
-      var gifRating = "R"
-      var aGif = msg.split(" ") // because it's not lowercase
-      var gifTag = "emu" // cat or kitten
-      var gifLoc = 0
-      for (var i = 0; i < aGif.length; i++) {
-        if (aGif[i] == "!gif") {
-          gifLoc = i+1
-        }
-      }
-
-      for (var i = gifLoc; i < aGif.length; i++) {
-        gifTag = gifTag + "%20" + aGif[i]
-      }
-
-      var url = "https://api.giphy.com/v1/gifs/random?api_key=9nbVwPCSeiP6Hh17oPJkMCRnYpA99FUO&tag=" + gifTag + "&rating=" + gifRating;
-      logger.info("Giphy request URL: " + url)
-
-      request.get({
-        url: url,
-        json: true,
-        headers: {'User-Agent': 'request'}
-      }, (err, res, data) => {
-        if (err) {
-          logger.info("Giphy request failed")
-        } else if (res.statusCode !== 200) {
-          logger.info("Giphy request succeeded")
-        } else {
-          // data is already parsed as JSON:
-          var received = data
-          // loop through each data object because there can be more than one
-          for (var data in received) {
-            var giphy = received[data];
-            receivedMessage.channel.send(giphy.embed_url)
-            logger.info("<" + receivedMessage.channel.id + "> @catbot: " + giphy.embed_url)
-
-            // suppress output because the gif is the output
-            suppressOutput = true
-            // console.log(data+": "+received[data]);
-          }
-
-        }
-      });
+      emu.gif(receivedMessage.channel, msg);
     }
 
-    var retString = "...";
-    var retSquawk = "SQUAWK!"
-
-    // 5% chance to randomly squawk
-    var randSquawk = Math.random()
-    if (randSquawk < .05) {
-      retString = retSquawk
+    if (msg.toLowerCase().includes("!sticker")) {
+      silent = true
+      emu.sticker(receivedMessage.channel, msg);
     }
 
-    if (!(suppressOutput)) {
-      receivedMessage.channel.send(retString)
-    } else {
-      // squawk even if output is suppressed
-      // this can only happen when you request a gif
-      if (retString == retSquawk) {
-        receivedMessage.channel.send(retString)
-      }
+    if (!(silent)) {
+      emu.reply(receivedMessage.channel, receivedMessage.content)
     }
-
-    logger.info("<" + receivedMessage.channel.id + "> @emuji: " + retString)
-
   }
 })
 
-client.login(bot_secret_token)
+client.login(bot_secret.bot_secret_token)
