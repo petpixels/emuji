44b0fc73171c8b7db587a0165d6ff0602e40615e
root <Alan Underwood>
2019-03-14 05:04:28 +0000

    Updated paths in example

---
+++ bot-example.js
@@ -0,0 +1,17 @@
+var emubot = require("./lib/bot");
+const emuji = require("./lib/emuji-functions")
+
+var emubot = new emubot();
+
+emubot.name("Emu")
+emubot.keywords("emu")
+emubot.default_reply("...")
+emubot.log("Log test: emu");
+emubot.rating("PG-13")
+emubot.gif()
+emubot.sticker()
+emubot.log(emuji.react("shop"))
+emubot.play()
+
+//goatbot.odds(".25")
+emubot.reply()
